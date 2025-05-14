79b9b4feec67dea70ea97180d573458f4e149715
saidovictik <saidovictiktok80@gmail.com>
2025-05-14 22:09:10 +0100

    Add files via upload

---
+++ reset.sh
@@ -0,0 +1,7 @@
+sudo cp /workspace/project/.freeCodeCamp/reset_files/kitty_ipsum_1.txt /workspace/project
+sudo cp /workspace/project/.freeCodeCamp/reset_files/kitty_ipsum_2.txt /workspace/project
+sudo cp /workspace/project/.freeCodeCamp/reset_files/stdout.txt /workspace/project
+sudo cp /workspace/project/.freeCodeCamp/reset_files/stderr.txt /workspace/project
+sudo cp /workspace/project/.freeCodeCamp/reset_files/name.txt /workspace/project
+sudo cp /workspace/project/.freeCodeCamp/reset_files/script.sh /workspace/project
+sudo cp /workspace/project/.freeCodeCamp/reset_files/kitty_info.txt /workspace/project
